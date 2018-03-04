95c75341193e3515d3ccb47cd4a8b2d4638accbb
Rojin BIjukchhe <35796647+rojin025@users.noreply.github.com>
2018-03-03 22:09:19 -0800

    to do list

--- JS Practise/js/script.js
+++ JS Practise/js/script.js
@@ -1,9 +1,25 @@
-var col = [ "red" , "orange", "black" , "pink", "rose"];
+var list = ["Buy New Turtule "];
+var i = 0;
+var input = prompt("What would you like to do?");
 
-function color() {
-  console.log("###########");
-  console.log(col);
-  console.log("###########");
+while(input !== "quit"){
+  if(input === "list") {
+    listTodos();
+  } else if(input === "delete"){
+    var index = prompt("Enter index of todo to delect.");
+    list.splice(index,i);
+    console.log("Successfully Delected");
+  } else if(input === "new") {
+    var newTodo = prompt("Enter New Todo");
+    list.push(newTodo);
+    console.log("Added New todos")
+  }
 }
 
-col.forEach(color);
+function listTodos () {
+  console.log("    *******To Do List's*******    ")
+  list.forEach(funtion(todo, i){
+    console.log(i + ": " + todo);
+  });
+  console.log("    **************************    ")
+}
