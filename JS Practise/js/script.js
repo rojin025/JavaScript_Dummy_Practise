a2c34f24c321805d39a53cd2de0ce457437a575b
Rojin BIjukchhe <35796647+rojin025@users.noreply.github.com>
2018-03-03 16:07:30 -0800

    v.1.3

--- JS Practise/js/script.js
+++ JS Practise/js/script.js
@@ -1,3 +1,21 @@
-var userName = prompt("What is your name?");
-alert("Again Nice to meet you " + userName);
-console.log("Nice to meet you " + userName);
+// var userFName = prompt("What is your First Name?");
+// var userLName = prompt("What is your Last Name?");
+// var userAge = prompt("What is your Age?");
+// alert("Your name is " + userFName + userLName + ".");
+// alert("Your age is " + userAge + ".");
+// console.log("Nice to meet you " + userFName + userLName);
+var a = 10;
+var b = "c";
+
+b === "c" || a >= 11;
+
+var x = 3;
+var y = 8;
+
+!(x == "3" || x === y) && !(y != 8 && x <= y)
+
+var str ="";
+var msg = "haha!"
+var isFunny = "false"
+
+!(( str || msg ) && isFunny)
